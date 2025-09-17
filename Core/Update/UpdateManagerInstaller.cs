1b40fb0babca96d53e85a1a03f9891226ea6d1b7
BartekNizio <bartek.nizio@gmail.com>
2025-09-17 09:08:19 +0200

    Test without assembly

---
+++ Core/Update/UpdateManagerInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Zenject;
+
+namespace BartekNizio.EntitasSystem
+{
+	public class UpdateManagerInstaller : MonoInstaller
+	{
+		public override void InstallBindings()
+		{
+			Container.Bind<UpdateSystem>().AsSingle();
+			Container.Bind<FixedUpdateSystem>().AsSingle();
+			var updateManager = FindObjectOfType<UpdateManager>();
+			Container.BindInstance( updateManager );
+		}
+	}
+}
---
+++ Core/Update/UpdateSystem.cs
@@ -0,0 +1,25 @@
+﻿namespace BartekNizio.EntitasSystem
+{
+	public class UpdateSystem : InjectableFeature
+	{
+		public UpdateSystem(Contexts contexts)
+		{
+			/*Add(new JobsFeature(contexts));
+			Add(new SystemSequenceFeature(contexts));
+			Add(new StateManagerFeature(contexts));
+
+			Add(new SceneFeature(contexts));
+			Add(new GameFlowFeature(contexts));
+			Add(new LevelMapFeature(contexts));
+			
+			Add(new InputFeature(contexts));
+			Add(new CameraFeature(contexts));
+			Add(new PlayerFeature(contexts));
+
+			Add(new GameEventSystems(contexts));
+			Add(new MetaEventSystems(contexts));
+			Add(new GameCleanupSystems(contexts));*/
+			//Add(new InputCleanupSystems(contexts));
+		}
+	}
+}
